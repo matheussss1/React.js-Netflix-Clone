ff1a8506b9d4609c95e1b3bee87b39d8d4e75a58
Matheus Carvalho <matheusfelipe_contato@hotmail.com>
2021-10-17 21:29:58 -0300

    feat: header muda de cor no scroll

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,9 +2,20 @@ import NetflixLogo from '../../assets/logo.png';
 import Avatar from '../../assets/avatar.png'
 
 import './style.css'
+import { useState } from 'react/cjs/react.development';
 
-export const Header = () => (
-    <header className="header">
+export const Header = () => {
+  const [header, setHeader] = useState(false);
+
+  window.addEventListener('scroll', handleChangeHeaderBackground);
+
+  function handleChangeHeaderBackground() {
+    if (window.scrollY >= 80) return setHeader(true);
+    return setHeader(false);
+  }
+
+  return (
+    <header className={`header ${header? 'header--active' : ''}`}>
         <div className="header__logo">
           <img src={NetflixLogo} alt="Site logo"/>
         </div>
@@ -13,4 +24,5 @@ export const Header = () => (
           <img src={Avatar}/>
         </div>
     </header>
-)
+  )
+}
