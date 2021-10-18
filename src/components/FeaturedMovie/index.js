232f886edefd4b52bdae3ac05ccb5ca4cd553c33
Matheus Carvalho <matheusfelipe_contato@hotmail.com>
2021-10-18 00:52:51 -0300

    feat: media query for mobile

--- src/components/FeaturedMovie/index.js
+++ src/components/FeaturedMovie/index.js
@@ -17,17 +17,17 @@ export const FeaturedMovie = ({movie}) => {
                         </h1>
                         <div className="featured-movie__info">
                             <span>
-                                {movie.first_air_date.slice(0,4)}
+                                {movie.first_air_date?.slice(0,4)}
                             </span>
                             <span>
                                 {`${movie.number_of_seasons} Temporada${movie.number_of_seasons > 1 ? 's' : ''}`}
                             </span>
                             <span>
-                                {movie.vote_average}
+                                {`${movie.vote_average} / 10`}
                             </span>
                         </div>
                         <p className="featured-movie__overview">
-                            {movie.overview}
+                            {movie.overview?.length > 250 ? `${movie.overview?.slice(0,250)} [...]` : movie.overview}
                         </p>
                     </div>
                 </div>
